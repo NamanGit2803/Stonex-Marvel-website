e6634e3827627542d2f98d081297547fdc502b7c
Naman <njain4282@gmail.com>
2025-09-13 09:46:50 +0530

    correct path

--- pages/our-design/PoojaRooms.jsx
+++ pages/our-design/PoojaRooms.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Hero from '@/components/home/Hero'
-import DreamTempleBudget from '@/components/home/DreamTempleBudget'
+import Hero from '@/components/Hero'
+import DreamTempleBudget from '@/components/DreamTempleBudget'
 import OurServices from '@/components/OurServices'
-import DimensionsPictures from '@/components/home/DimensionsPictures'
+import DimensionsPictures from '@/components/DimensionsPictures'
 
 const PoojaRooms = () => {
   return (
