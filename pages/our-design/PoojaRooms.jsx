08d7a9e7b9e2bd18113237a6e8e8682c495c61b7
Naman <njain4282@gmail.com>
2025-10-01 15:01:28 +0530

    staged

--- pages/our-design/PoojaRooms.jsx
+++ pages/our-design/PoojaRooms.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../../components'
+import Hero from '../../components/Hero'
 import DreamTempleBudget from '../../components/DreamTempleBudget'
 import OurServices from '../../components/OurServices'
 import DimensionsPictures from '../../components/DimensionsPictures'
